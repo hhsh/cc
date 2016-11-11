03fda05ec73a0dfa9ce83d4e484989ca38ca34e5
hhsh <310519804@qq.com>
2016-11-12 04:57:26 +0800

    add getint.c

---
+++ pointer/getint.c
@@ -0,0 +1,38 @@
+
+#include<stdio.h>
+
+ #include <ctype.h>
+int SIZE = 1000;
+
+
+int getch(void);
+void ungetch(int);
+
+int getint(int *pn)
+{
+    int c,sign;
+    while(isspace(c=getch()))
+        ;
+    if(!isdigit(c) && c!= EOF  && c !='+' && c != '-'){
+        ungetch(c);
+        return 0;
+    }
+    sign = (c == '-')? -1:1;
+    if(c == '+' || c =='-'){
+        c = getch();
+    }
+    for(*pn =0 ;isdigit(c);c = getch())
+        *pn = 10 * *pn + (c - '0');
+    *pn *= sign;
+    if(c != EOF)
+        ungetch(c);
+    return c ;
+} 
+ int main()
+{
+
+    int n,arr[SIZE],getint(int * );
+    for(n=0;n<SIZE && getint(&arr[n]) != EOF;n++)
+        ;
+    return 0;
+}
